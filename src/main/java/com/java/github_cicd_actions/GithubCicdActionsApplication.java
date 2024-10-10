f350065a1f5811e291b583e8afd72923529b7d53
Lalitha-git-project <lalithasanthosh11@gmail.com>
2024-10-10 16:42:06 +0530

    code

---
+++ src/main/java/com/java/github_cicd_actions/GithubCicdActionsApplication.java
@@ -0,0 +1,27 @@
+package com.java.github_cicd_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@Getmapping("/welcome")
+	public String welcome(){
+		return "WELCOME";
+	}
+
+	/*echo "# gitup-actions-example" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/Lalitha-git-project/gitup-actions-example.git
+	git push -u origin main */
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
