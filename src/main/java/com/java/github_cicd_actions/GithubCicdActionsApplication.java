762fede958eb0fed8ae8bbd79b1a8c9e06fcf0e3
Lalitha-git-project <lalithasanthosh11@gmail.com>
2024-10-10 18:28:36 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/java/github_cicd_actions/GithubCicdActionsApplication.java
+++ src/main/java/com/java/github_cicd_actions/GithubCicdActionsApplication.java
@@ -3,6 +3,7 @@ package com.java.github_cicd_actions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
