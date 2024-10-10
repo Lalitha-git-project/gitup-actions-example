119c1bff5563d00a225c1632eeb76cf8c10c3ccb
Lalitha-git-project <lalithasanthosh11@gmail.com>
2024-10-10 18:20:50 +0530

    workflow change

--- src/main/java/com/java/github_cicd_actions/GithubCicdActionsApplication.java
+++ src/main/java/com/java/github_cicd_actions/GithubCicdActionsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GithubCicdActionsApplication {
 
-	@Getmapping("/welcome")
+	@GetMapping("/welcome")
 	public String welcome(){
 		return "WELCOME";
 	}
